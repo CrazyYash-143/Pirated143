206d032fbc096745d95c8f3ad2ea89aab6b3eb69
Pirated143 <71497650+CrazyYash-143@users.noreply.github.com>
2021-07-15 18:39:39 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,21 +11,21 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
+  username: "Pirated14e",
   title: "Hi all, I'm Saad",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A Newbie Coder , Cracker , Gamer , Youtuber , Logo Maker Many More Skills At This Age 😅."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing",
-  displayGreeting: true // Set false to hide this section, defaults to true
+  displayGreeting: false // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
+  github: "https://github.com/CrazyYash-143",
+  liin: "https://www.linkedin.com/in/saadpasta/",
   gmail: "saadpasta70@gmail.com",
   gitlab: "https://gitlab.com/saadpasta",
   facebook: "https://www.facebook.com/saad.pasta7",
